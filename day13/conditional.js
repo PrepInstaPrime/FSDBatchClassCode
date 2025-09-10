1dd517582769a8487460d1f8aa679c311ccf35ca
PrepInstaPrime <gaurav.pandey@addaeducation.com>
2025-09-10 20:20:18 +0530

    day14

--- day13/conditional.js
+++ day13/conditional.js
@@ -1,3 +1,5 @@
+// Example 1
+/*
 let age=17;
 if(age>=18){
     console.log("you are eligible to vote");
@@ -5,6 +7,31 @@ if(age>=18){
 else{
     console.log("You are not eligible to vote");
 }
+*/
+// Example 2: 
+/*
+let marks=94;
+if(marks>=90){
+    console.log("A");
+}else if(marks>=80){
+    console.log("B");
+}else if(marks>=70){
+    console.log("C");
+}else if ( marks>=60){
+    console.log("D")
+}else {
+    console.log("fail");
+}
+*/
 
-// output : windows:  ctrl + /
-// mac : cmd + /
+// Example 3: 
+let fruit= "Apple1";
+switch(fruit){
+    case "Apple" : console.log("Apple : Rs. 90");
+    break;
+    case "Mango" : console.log("Mango : Rs. 60 ");
+    break;
+    case "Orange" : console.log("Orange : Rs. 50");
+    break;
+    default : console.log("Not available or invalid input");
+}
---
+++ day14/ObjectEx.js
@@ -0,0 +1,12 @@
+let mobile={
+    name:"Samsung",
+    price:50000,
+    model : "S23"
+}
+// console.log(mobile.name)
+// console.log(mobile.price)
+// what if i want to access all the keys 
+// for in loop
+for ( let key in mobile){
+    console.log(key +" : " +mobile[key]) // mobile."name"
+}
